4d05f2baf07615f2d1b6a06b7efb6b8da8dbce3a
Bernardo Rosa Pinheiro <bedarosa99@gmail.com>
2022-12-09 00:16:50 -0300

    initial commit

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,65 @@
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+        // api/projects?query=netcore
+        [HttpGet]
+        public IActionResult Get(string query)
+        {
+            // buscar todos os filtrar
+            return Ok();
+        }
+
+        // api/projects/3
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            // Buscar o projeto
+
+            // return NotFound();
+
+            return Ok();
+        }
+
+        // api/projects
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+
+        // api/projects/2
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            // Atualizo objeto
+
+            return Ok();
+        }
+
+        // api/projects/2
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir retorna NotFound
+
+            // Remover
+
+            return NoContent();
+        }
+    }
+}
