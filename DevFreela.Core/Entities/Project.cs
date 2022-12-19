81e64f4a8e410d0a88e7f946aa2db28b2789900f
bedarosa <bedarosa99@gmail.com>
2022-12-19 09:21:39 -0300

    Camada Infrastructure - Parte 2

---
+++ DevFreela.Core/Entities/Project.cs
@@ -0,0 +1,31 @@
+﻿using DevFreela.Core.Enums;
+
+namespace DevFreela.Core.Entities
+{
+    public class Project : BaseEntity
+    {
+        public Project(string title, string description, int idCliente, int idFreelancer, decimal? totalCoast)
+        {
+            this.title = title;
+            Description = description;
+            IdCliente = idCliente;
+            IdFreelancer = idFreelancer;
+            TotalCoast = totalCoast;
+
+            CreatedAt = DateTime.Now;
+            Status = ProjectStatusEnum.Created;
+            Comments = new List<ProjectComent>();
+        }
+
+        public string title { get; private set; }
+        public string Description { get; private set; }
+        public int IdCliente { get; private set; }
+        public int IdFreelancer { get; private set; }
+        public decimal? TotalCoast { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime? StartedAt { get; private set; }
+        public DateTime FinishedAt { get; private set; }
+        public ProjectStatusEnum Status { get; private set; }
+        public List<ProjectComent> Comments { get; private set; }
+    }
+}
